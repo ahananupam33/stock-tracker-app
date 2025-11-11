82be35b6bc5cb81047a47e8397592e6e8046a978
Ahan Anupam <68801494+ahananupam33@users.noreply.github.com>
2025-11-12 01:37:36 +0530

    Correct page.tx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import TradingViewWidget from "@/components/TradingViewWidget"
 import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from "@/lib/constants"
 
-const Home: () => {
+const Home = () => {
   const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
 
   return (
